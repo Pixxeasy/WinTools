d09cdf02352ee76fc6a90c64548567fcd0d9143e
gitagogaming <76603653+gitagogaming@users.noreply.github.com>
2022-03-02 21:21:09 -0500

    Update util.py

--- src/utils/util.py
+++ src/utils/util.py
@@ -510,10 +510,10 @@ def getAllOutput_TTS2():
     return audio_dict
 
 
-
-def rotate_display(display_num, rotate_choice):
+def rotate_display2(display_num, rotate_choice):
     display_num = display_num.split(":")[0]
-    display_num = display_num -1
+    display_num = int(display_num) -1
+   # print(rotate_choice)
     rotation_val=""
     if (rotate_choice != None):
         if (rotate_choice == "180"):
@@ -525,13 +525,39 @@ def rotate_display(display_num, rotate_choice):
         else:
             rotation_val=win32con.DMDO_DEFAULT
 
-    device = win32api.EnumDisplayDevices(None,display_num)
+    device = win32api.EnumDisplayDevices(None, int(display_num))
     dm = win32api.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
     if((dm.DisplayOrientation + rotation_val)%2==1):
         dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth   
     dm.DisplayOrientation = rotation_val
 
     win32api.ChangeDisplaySettingsEx(device.DeviceName,dm)
+    
+
+
+#def rotate_display(display_num, rotate_choice):
+#    print("THE NUMBER", display_num)
+#    display_num = display_num.split(":")[0]
+#    print("THE NUMBER", display_num)
+#   # display_num = display_num -1
+#    rotation_val=""
+#    if (rotate_choice != None):
+#        if (rotate_choice == "180"):
+#            rotation_val=win32con.DMDO_180
+#        elif(rotate_choice == "90"):
+#            rotation_val=win32con.DMDO_270
+#        elif (rotate_choice == "270"):   
+#            rotation_val=win32con.DMDO_90
+#        else:
+#            rotation_val=win32con.DMDO_DEFAULT
+#
+#    device = win32api.EnumDisplayDevices(None,display_num)
+#    dm = win32api.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
+#    if((dm.DisplayOrientation + rotation_val)%2==1):
+#        dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth   
+#    dm.DisplayOrientation = rotation_val
+#
+#    win32api.ChangeDisplaySettingsEx(device.DeviceName,dm)
     
     
 
